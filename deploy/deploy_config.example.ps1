2af99e731a94a0d4e8f9c33aa11971d94717ae2a
ISGPanel <isgpanel@kullanici.com>
2026-07-10 13:37:19 +0300

    Update ISGPanel

--- deploy/deploy_config.example.ps1
+++
@@ -1,4 +0,0 @@
-$DeployHost = "SUNUCU_IP_VEYA_DOMAIN"
-$DeployUser = "root"
-$RemoteProjectPath = "/var/www/isgpanel"
-$RemoteDeployScript = "$RemoteProjectPath/deploy_server.sh"
--- deploy/deploy_config.ps1
+++
@@ -1,8 +0,0 @@
-$DeployHost = "46.224.122.18"
-$DeployUser = "root"
-
-# Bu yol Windows'taki lokal klasor degil, SSH ile girdikten sonra sunucudaki proje klasoru olmali.
-# Sunucuda proje klasorundeyken `pwd` komutu ile bulunur.
-$RemoteProjectPath = "/var/www"
-
-$RemoteDeployScript = "$RemoteProjectPath/deploy/deploy_server.sh"
