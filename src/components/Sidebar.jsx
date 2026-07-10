853ddef3847f63f9311bae8b353f6e683aa81612
ISGPanel <isgpanel@kullanici.com>
2026-07-10 13:38:05 +0300

    Update ISGPanel

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -88,8 +88,8 @@ const isPaymentPending =
     },
 
     {
-      name: "Firmalarım",
-      path: "/panel/firmalarım",
+      name: "Firmalar",
+      path: "/panel/firmalar",
       icon: Building2,
     },
 
